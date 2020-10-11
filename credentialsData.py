d4f877d0714c883c6653f53558a1bae9a78c93e0
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-11 20:09:48 +0300

    passed in three argurments in the constructor in credentialsData class

--- credentialsData.py
+++ credentialsData.py
@@ -1,1 +1,11 @@
 class CredentialsData:
+    """
+    create new instances
+    """
+
+    credentials = []
+     def __init__(self, platform, username, password):
+
+        self.platform = platform
+        self.username = username
+        self.password = password
