e37bffc1bf10c9b0c65820673de1908105de0241
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-12 05:27:22 +0300

    add a setup method for the TestuserData class

--- test_userData.py
+++ test_userData.py
@@ -1,2 +1,9 @@
 import unittest
 from userData import UserData
+
+class TestuserData(unittest.TestCase):
+    def setUp(self):
+        """
+        set up method
+        """
+        self.new_user = UserData("cliff", "ochieng", "cliffochieng844@gmail.com", "cliffochieng", "yoloyolo") 
