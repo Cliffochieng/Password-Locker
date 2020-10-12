85fdec777e5ee3e7264166ff96153da824706a03
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-12 05:07:38 +0300

    add an init method to initialize the userData class

--- userData.py
+++ userData.py
@@ -1,1 +1,18 @@
 class UserData:
+
+    '''
+    class to create new user accounts 
+    '''
+    create_account = []
+
+    def __init__(self, firstName, lastName, email, username, password):
+
+        """
+        Initializes the class
+        """
+
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.username = username
+        self.password = password
