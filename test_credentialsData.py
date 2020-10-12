cab3e6a9cbb94037a1f54433225a1b1e1b938c82
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-12 04:52:38 +0300

    add a test init case to test if the object is correctly initialized

--- test_credentialsData.py
+++ test_credentialsData.py
@@ -8,3 +8,11 @@ class TestCredentials(unittest.TestCase):
         setUp method
         """
         self.new_credential = CredentialsData("Gmail", "cliff", "lolololo") 
+
+    def test_init(self):
+        """
+        testing initialization
+        """
+        self.assertEqual(self.new_credential.platform, "Gmail")
+        self.assertEqual(self.new_credential.username, "cliff")
+        self.assertEqual(self.new_credential.password, "yoloyolo")
