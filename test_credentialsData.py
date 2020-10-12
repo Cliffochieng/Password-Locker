8a8d821935f29b73ff38f73797a81b56af291aca
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-12 04:48:43 +0300

    add a setup method to the credentialData

--- test_credentialsData.py
+++ test_credentialsData.py
@@ -1,3 +1,10 @@
 import unittest
 
 from credentialsData import CredentialsData
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        """
+        setUp method
+        """
+        self.new_credential = CredentialsData("Gmail", "cliff", "lolololo") 
