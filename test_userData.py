00a8209a4258ef7d139c50b5aa780a3ed079077c
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-12 05:31:16 +0300

    add a test init method for initializing  the TestuserData class

--- test_userData.py
+++ test_userData.py
@@ -7,3 +7,13 @@ class TestuserData(unittest.TestCase):
         set up method
         """
         self.new_user = UserData("cliff", "ochieng", "cliffochieng844@gmail.com", "cliffochieng", "yoloyolo") 
+
+    def test_init(self):
+        """
+        testing initialization of class
+        """
+        self.assertEqual(self.new_user.firstName, "cliff")
+        self.assertEqual(self.new_user.lastName, "kasera")
+        self.assertEqual(self.new_user.email, "ckasera@gmail.com")
+        self.assertEqual(self.new_user.username, "Blankphrase")
+        self.assertEqual(self.new_user.password, "lololo")
