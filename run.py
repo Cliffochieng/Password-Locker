8842aae5ba08f67ae887436bc0fdeae71471c0e2
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-12 05:59:15 +0300

    add a new users method that create a new user

--- run.py
+++ run.py
@@ -5,3 +5,12 @@ import string
 from userData import UserData
 from credentialsData import CredentialsData
 
+
+def new_users(name_one, name_two, email_address, user_name, pass_word):
+
+    """
+    creates new user 
+    """
+    new_user = UserData(name_one, name_two, email_address, user_name, pass_word)
+    
+    return new_user
