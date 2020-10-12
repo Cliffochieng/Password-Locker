ee1969c4abc14bef09128b333b244a2c42753bd5
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-12 10:46:48 +0300

    add the main() that allows for the interactivity between the user and the program

--- run.py
+++ run.py
@@ -67,3 +67,116 @@ def delete_credentials(credential):
     delete a credential
     """
     credential.delete_credential()
+
+def main():
+    print("\n")
+    print("Welcome to Password Locker")
+    print("This will store your credentails and generate a password for you")
+
+    while True:
+        print("Type register to create account. If you already created one, type signin.")
+        account_login = input()
+        if account_login == "register":
+            print("First name:")
+            firstname = input()
+            print('\n')
+            print("Last name:")
+            lastname = input()
+            print('\n')
+            print("Username:")
+            username = input()
+            print('\n')
+            print("email:")
+            emails = input()
+            print('\n')
+            print("To create a password, type create or to generate a password type generate")
+            pass_choice = input()
+            while True:
+                if pass_choice == "create":
+                    print("Password:")
+                    password = input()
+                    break
+                elif pass_choice == "generate":
+                    password = randompassword()
+                    print('\n')
+                    break
+                else:                    
+                    print("Type create or 'generate")
+                    break
+            save_accounts(new_users(firstname, lastname, emails, username, password))
+            print("Created successfully.")
+            print(f" Username: {username}, password: {password}.")
+            print('\n')
+            break
+
+        elif account_login == "signin":
+            print("Sign In:\n")
+            print("Username:")
+            username = input()
+            print('\n')
+            print("Password:")
+            password = input()
+            print('\n')
+            sign_in = check_user(username, password)
+            if sign_in == True:
+                break
+            print("Please sign up to access password locker.\n")
+
+        else:
+            print("Try the choices above")
+
+    while True:
+        print(f"Type create to add a credential, saved to see the saved credentials or exit to stop adding credentials.")
+
+        credentials2 = input()
+
+        if credentials2 == 'create':
+            print("Type platform to add:")
+            plat_form = input()
+            print("\n")
+
+            print("Type in username for the platform:")
+            username2 = input()
+
+            print("To create a password, type create or to generate a password type generate")
+            pass_choice = input()
+            while True:
+                if pass_choice == "create":
+                    print("Password:")
+                    password2 = input()
+                    break
+                elif pass_choice == "generate":
+                    password2 = randompassword()
+                    print('\n')
+                    break
+                else:
+                    print("Type create or 'generate")
+                    break
+
+            save_credentials(add_credential(plat_form, username2, password2))
+            print('\n')
+            print(f" {plat_form}: {username2}: {password2}")
+            print('\n')
+
+        elif credentials2 == 'saved':
+            print ("Enter your password: ")
+            credPassword = input()
+            print ("\n")        
+            if credPassword == password:       
+                display_credential()
+                print("Credentials:\n")
+                for credential in display_credential():
+                    print(f"Platform => {credential.platform}: Username => {credential.username}: Password => {credential.password}")
+                    print('\n')
+            else:
+                print("There are no more credentials saved for now. Type create to create a credential.")
+                print('\n')
+        elif credentials2 == 'exit':
+                    break
+        else:
+            print("Sorry, try again.")
+            print('\n')
+
+
+if __name__ == '__main__':
+    main()
